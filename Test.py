6ddb4770c7f835e1958680279c2c82885f285efb
LavdrimScankabber <lav@scankab.dk>
2025-04-09 08:39:40 +0200

    tilføjet Test.py med valg 1 og 2

---
+++ Test.py
@@ -0,0 +1,11 @@
+print("Tryk 1 for at skrive 'test'")
+print("Tryk 2 for at skrive '2xtest'")
+
+valg = input("Dit valg: ")
+
+if valg == "1":
+    print("test")
+elif valg == "2":
+    print("2xtest")
+else:
+    print("Ugyldigt valg. Prøv igen.")
